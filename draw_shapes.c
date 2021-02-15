53af5bebb12db6952cebab687ce5d6296a573b60
Daniel Solorzano <dsolorzano@miners.utep.edu>
2021-02-14 20:39:24 -0700

    Added arrow shape and character sizes

--- draw_shapes.c
+++ draw_shapes.c
@@ -11,6 +11,8 @@ void print_square(int leftCol, int size)
     for (col = 0; col < leftCol; col++) putchar(' ');
     for (       ; col < endCol;  col++) putchar('*');
     putchar('\n');
+
+    
   }
 }
 
@@ -25,4 +27,29 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
+
+void print_arrow(int leftCol, int size)
+{
+  
+  int i, j;
+  int endCol = leftCol + size;
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol +size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+  for (int row = 0;row < size; row++){
+    int col;
+    for (col = -3;col < leftCol; col++) putchar(' ');
+    for (       ; col < endCol;  col++) putchar('*');
+    putchar('\n');
+  }
+   
+    
+}    
+    
+  
+  
 
